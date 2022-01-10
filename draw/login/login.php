f54affd736a4c9c07cb884ef111d4e04cd79259e
bellaprimin <91933278+bellaprimin@users.noreply.github.com>
2022-01-11 01:26:46 +0700

    menambah file kodingan login admin dan user

---
+++ draw/login/login.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>aplikasi Drawpen</title>
+  <style>
+    h1{
+      color: rgb(3, 28, 69);
+      text-align: center;
+      font-size: 90px;
+      text-indent: 600px;
+    }
+    h6{
+      color: rgb(3, 28, 69);
+      text-align: center;
+      font-size: 40px;
+      text-indent: 800px;
+    }
+    body{
+      background-image:url("444.png");
+      background-repeat: no-repeat;
+      background-size: 1200px;
+    }
+    table{
+        margin-right: 280px;
+    }
+     
+  </style>
+</head>
+<body >
+    <h1>Drawing</h1>
+    <br>
+    <br>
+    <td>
+        <table class="tabel" align="center" style="border-radius: 15px; background-color: rgb(75, 159, 233);" cellspacing="15">
+            <tr class="trt1" height="50px" bgcolor="#E0FFFF">
+                <td colspan="2" align="center" style="color: #2412b0"> <h3>LOGIN</h3></td>
+            </tr>
+            <tr>
+                <td>e-mail or username:</td>
+                <td>:
+                    <input name="e-mail" id="NPM" type="text" placeholder="masukkan e-mail" maxlength="10" required>
+                </td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td>:
+                    <input name="Password" id="Password" type="password" placeholder="masukkan Password ">
+                </td>
+            </tr>
+            <tr>
+                <td><a href="home_admin.php">LOGIN ADMIN</a></td>
+                <td><a href="home.html">LOGIN</a></td>
+                <td><a href="daftar.html">DAFTAR</a></td>
+            </tr>
+        </table>
+    </td>
+    <td>
+
+    </td>
+</tr>
+<tr height="90px">
+    <td>
+    <br>
+    <br>
+    <br>
+    <h6>Easy Online to Draw and Painting</h6>
+  </body>
+</head>
+</html>
